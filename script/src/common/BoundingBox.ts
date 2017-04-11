16f46f40594f3cb0561dd67cc9ffa7b53ac5c31e
Lucas Tavares <lucas.rocha@lucida.com.br>
2017-04-11 17:41:36 -0300

    Padding

--- script/src/common/BoundingBox.ts
+++ script/src/common/BoundingBox.ts
@@ -1,4 +1,5 @@
 import { Component } from './Component'
+import { Collidable } from './Collidable'
 
 export class BoundingBox extends Component {
 	
@@ -6,26 +7,56 @@ export class BoundingBox extends Component {
 	private _originY : number
 	private _endX : number
 	private _endY : number
+	private _paddingTop : number;
+	private _paddingRight : number;
+	private _paddingBottom : number;
+	private _paddingLeft : number;
 
-	constructor() {
+	constructor(padding?) {
 		super()
+
+		if (padding) {
+			this.padding = padding;
+		}
+		else {
+			this.padding = 0;
+		}
+	}
+
+	set paddingTop(value : number) {
+		this._paddingTop = value
+	}
+
+	set paddingRight(value : number) {
+		this._paddingRight = value
+	}
+
+	set paddingBottom(value : number) {
+		this._paddingBottom = value
+	}
+
+	set paddingLeft(value : number) {
+		this._paddingLeft = value
+	}
+
+	set padding(value : number) {
+		this._paddingTop 	= value
+		this._paddingRight 	= value
+		this._paddingLeft 	= value
+		this._paddingBottom = value
 	}
 
 	get base() : number {
-		const DIFF = this._originX - this._endX
-		return Math.abs(DIFF)
+		const BASE = (this._paddingLeft + this._originX) - (this._endX + this._paddingRight)
+		return Math.abs(BASE)
 	}
 
 	get height() : number {
-		const DIFF = this._originY - this._endY
-		return Math.abs(DIFF)
+		const HEIGHT = (this._paddingTop + this._originY) - (this._endY + this._paddingBottom)
+		return Math.abs(HEIGHT)
 	}
 
-	isOverlapping(other : BoundingBox) : boolean {
-		if ((other._endX > this._originX && other._originX < this._endX)
-		|| (other._endY > this._originY && other._originY < this._endY)) {
-			return true
-		}
+	isCollidingWith(target : Collidable) : boolean{
 
 		return false
 	}
